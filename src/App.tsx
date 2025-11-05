16c63c18d762a8bdfc2bbedc63d0ace0d77b5a76
Daniela Cardenas <dam.cardenas@yavirac.edu.ec>
2025-11-05 14:51:41 -0500

    Cambios Ander xd

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
+import Card from "./components/card";
+
 function App() {
-  return (
-    <div className="container text-center mt-5">
-      <h1 className="text-primary">👋 ¡Hola A TODOS CON BOOTSTRAP!</h1>
-      <p className="lead mt-3">WASAAAAAAAAAAAAAA</p>
-      <button className="btn btn-success mt-3">Haz clic aquí para conocer todo </button>
-    </div>
-  );
+  return <Card />;
 }
 
 export default App;
